c9bce4df4cde809ef7226a7f2c54ab5e1b3b9a90
Younes Darabi <younes.darabi@outlook.com>
2026-04-02 09:58:47 +0200

    add get business and customer

--- user_profile/api/urls.py
+++ user_profile/api/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 
-from .views import ProfileDetailView
+from .views import ProfileDetailView, BusinessListView, CustomerListView
+
 
 urlpatterns = [
-    path('<int:pk>/', ProfileDetailView.as_view(), name='profile-detail')
+    path('<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
+    path('business/', BusinessListView.as_view(), name='profile-business'),
+    path('customer/', CustomerListView.as_view(), name='profile-customer')
 ]
--- user_profile/api/views.py
+++ user_profile/api/views.py
@@ -1,13 +1,31 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+
 from user_auth.models import CustomUser
-from .serializers import ProfileSerializer
-from rest_framework.response import Response
-from rest_framework import status
+from .serializers import ProfileSerializer, BusinessListSerializer, CustomerListSerializer
+from .permissions import IsOwner
 
 
-class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
 
     queryset = CustomUser.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'PATCH':
+            return [IsAuthenticated(), IsOwner()]
+        return [IsAuthenticated()]
+    
+
+class BusinessListView(generics.ListAPIView):
+
+    queryset = CustomUser.objects.filter(type = 'business-user')
+    serializer_class = BusinessListSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CustomerListView(generics.ListAPIView):
+
+    queryset = CustomUser.objects.filter(type = 'customer')
+    serializer_class = CustomerListSerializer
     permission_classes = [IsAuthenticated]
