837feebe3bf6df7506edb92babb7c5cbb8d664db
Younes Darabi <younes.darabi@outlook.com>
2026-04-05 21:05:34 +0200

    add permission ISUserBusiness

--- offers/api/views.py
+++ offers/api/views.py
@@ -1,12 +1,17 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .serializers import OfferSerializer
 from ..models import Offer
+from .permissions import ISUserBusiness
 
 
 class OfferView(generics.ListCreateAPIView):
 
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-    permission_classes = [AllowAny]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated(), ISUserBusiness()]
