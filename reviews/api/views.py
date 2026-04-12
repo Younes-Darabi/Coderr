1d151fcecc6c3201216b8f6c22cd75b23d42a218
Younes Darabi <younes.darabi@outlook.com>
2026-04-12 09:11:57 +0200

    update: reviews(view)

--- reviews/api/views.py
+++ reviews/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework import serializers
 
 from ..models import Review
 from .serializers import ReviewSerializer
@@ -23,7 +24,12 @@ class ReviewView(generics.ListCreateAPIView):
         return [IsAuthenticated(), IsCustomer()]
     
     def perform_create(self, serializer):
-        return serializer.save(reviewer=self.request.user)
+        business_user = serializer.validated_data['business_user']
+    
+        if Review.objects.filter(reviewer=self.request.user, business_user=business_user).exists():
+            raise serializers.ValidationError("You have already submitted a review for this business user.")
+    
+        serializer.save(reviewer=self.request.user)
     
 
 class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
